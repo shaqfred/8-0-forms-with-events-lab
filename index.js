535c9b8407a2a5ec8570ac45c8f5eb6bebe683d4
shaquala <96317236+shaqfred@users.noreply.github.com>
2022-12-16 13:56:03 -0500

    passed 2 tests still a working progress

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("Code your solution!")
+let input = document.querySelector("input");
+let form = document.querySelector("form");
+let unorderList = document.querySelector("ul");
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+})
+// * find text * //
+// * create new li * //
+// * add text to li * //
+// * add li to ul * //
+
+let text = input.value
+let listItem = document.createElement("li");
+listItem.innerText = text
+unorderList.append(listItem)
+
+listItem.addEventListener("click", (e) => {
+    listItem.style.textDecoration = "line-through"
+})
