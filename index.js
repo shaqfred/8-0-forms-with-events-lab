d5ff5f7824ee4fb160cf48e90cf95924a9275818
shaquala <96317236+shaqfred@users.noreply.github.com>
2022-12-29 21:44:32 -0500

    still passing only 2 tests

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ console.log("Code your solution!")
 let input = document.querySelector("input");
 let form = document.querySelector("form");
 let unorderList = document.querySelector("ul");
+let list =document.querySelector("list")
+
+
 
 form.addEventListener("submit", (e)=> {
     e.preventDefault();
+   
+   
 })
 // * find text * //
 // * create new li * //
 // * add text to li * //
 // * add li to ul * //
+
+ input = document.createElement("input")
+input.addEventListener("click", (e) =>{
+    e.preventDefault();
+    
+    input.style.textDecoration ="line-through"
+    input.append ($li)
+   
+
+ 
+}
+)
+
+
+
+
+
+
 
 let text = input.value
 let listItem = document.createElement("li");
@@ -18,4 +41,7 @@ unorderList.append(listItem)
 
 listItem.addEventListener("click", (e) => {
     listItem.style.textDecoration = "line-through"
+
+    listItem.append(input);
 })
+ 
